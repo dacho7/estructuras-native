ac547f847dd0d1f47b4186e89f98f90605408d14
dacho7 <davidtobarst@gmail.com>
2022-05-12 12:02:53 -0500

    navegacion funcional

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 //import { StatusBar } from "expo-status-bar";
 import React from "react";
+import "react-native-gesture-handler";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Login from "./src/components/Login.jsx";
+import Register from "./src/components/Register.jsx";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +15,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
