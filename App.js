2aaea1be92aae46023dd7b68a2f62b0a80dbb63d
dacho7 <davidtobarst@gmail.com>
2022-05-12 11:49:13 -0500

    install navigation stack

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 //import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Login from "./src/components/Login.jsx";
 
+const Stack = createStackNavigator();
+
 export default function App() {
-  return <Login />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
