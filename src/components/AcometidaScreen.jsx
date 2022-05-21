6d3665a0d8035620ce2876434ee26d31a0ed2735
dacho7 <davidtobarst@gmail.com>
2022-05-21 14:34:57 -0500

    Add componet list selectly

--- src/components/AcometidaScreen.jsx
+++ src/components/AcometidaScreen.jsx
@@ -1,56 +1,80 @@
 import React from "react";
-import { StyleSheet, Text, TextInput, ScrollView, SafeAreaView, TouchableOpacity, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+
+import RNPickerSelect from "react-native-picker-select";
+
 import { AcometidaModel } from "../models/Acometida";
 
 export default function AcometidaScreen() {
-
-  const databaseDates= AcometidaModel[0]
-  const fields = AcometidaModel[1]
-  const values = {}
+  const databaseDates = AcometidaModel[0];
+  const fields = AcometidaModel[1];
+  const values = {};
 
   fields.forEach((val) => {
-    values[val.name] = ""
-  })
+    values[val.name] = "";
+  });
 
-  const cambiarValor = (valor,name) => {
+  const cambiarValor = (valor, name) => {
     values[name] = valor;
-  }
+  };
 
   const validarCampos = () => {
-    let valid = true
+    let valid = true;
     Object.values(values).forEach((val) => {
-      if (val === ""){
-        valid = false
+      if (val === "") {
+        valid = false;
       }
-    })
-    return valid
-  }
+    });
+    return valid;
+  };
 
   const registrarDatos = () => {
-    if(!validarCampos()){
-      Alert.alert("Por favor ingresar todos los datos")
-    } else{
-      Alert.alert("Exito")
+    if (!validarCampos()) {
+      Alert.alert("Por favor ingresar todos los datos");
+    } else {
+      Alert.alert("Exito");
     }
-  }
+  };
 
   return (
     <SafeAreaView style={{ padding: 12 }}>
       <ScrollView style={{ marginTop: 12 }}>
-        {
-          fields.map((e, index) => 
-            <TextInput 
-              key={index}
-              placeholder={`Ingrese ${e.label}`}
-              style={styles.input}
-              value={values.name}
-              onChangeText={(text) => cambiarValor(text, e.name)}
-            ></TextInput>
-          )
-        }
+        {fields.map((e, index) => {
+          if (e.type === "string" || e.type === "real") {
+            return (
+              <TextInput
+                key={index}
+                placeholder={`Ingrese ${e.label}`}
+                style={styles.input}
+                value={values.name}
+                onChangeText={(text) => cambiarValor(text, e.name)}
+              ></TextInput>
+            );
+          }
+          if (e.type === "list") {
+            return (
+              <RNPickerSelect
+                style={styles.input}
+                key={index}
+                onValueChange={(value) => console.log(e)}
+                items={e.values}
+              ></RNPickerSelect>
+            );
+          }
+        })}
         <TouchableOpacity style={styles.button}>
-          <Text style={{ fontWeight: "500", color: "white" }}
-          onPress={registrarDatos}>
+          <Text
+            style={{ fontWeight: "500", color: "white" }}
+            onPress={registrarDatos}
+          >
             {databaseDates.registerLabel}
           </Text>
         </TouchableOpacity>
